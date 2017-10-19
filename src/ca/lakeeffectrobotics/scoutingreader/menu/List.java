68eafc8f58196ff29a1079206639b7522c5c575e
thebestusernameever <ajay@ajayinkingston.com>
2017-10-19 16:34:32 -0400

    started work on menus

---
+++ src/ca/lakeeffectrobotics/scoutingreader/menu/List.java
@@ -0,0 +1,16 @@
+package ca.lakeeffectrobotics.scoutingreader.menu;
+
+import java.awt.Graphics2D;
+
+public class List extends Menu{
+	
+	@Override
+	public void update(){
+		
+	}
+	
+	@Override
+	public void render(Graphics2D g){
+		
+	}
+}
---
+++ src/ca/lakeeffectrobotics/scoutingreader/menu/Menu.java
@@ -0,0 +1,13 @@
+package ca.lakeeffectrobotics.scoutingreader.menu;
+
+import java.awt.Graphics2D;
+
+public class Menu {
+	public void update(){
+		
+	}
+	
+	public void render(Graphics2D g){
+		
+	}
+}
