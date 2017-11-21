84fbdfd61a6cd65289009664c26fb26a7caafd67
ramachandra <ajay@ajayinkingston.com>
2017-11-20 20:48:33 -0500

    fix merge conflicts

--- src/ca/lakeeffectrobotics/scoutingreader/gui/MainFrame.java
+++ src/ca/lakeeffectrobotics/scoutingreader/gui/MainFrame.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package ca.lakeeffectrobotics.scoutingreader.gui;
 
 import javax.swing.JFrame;
@@ -36,39 +35,3 @@ public class MainFrame extends JFrame {
 //		add(new RobotPanel());
 	}
 }
-=======
-package ca.lakeeffectrobotics.scoutingreader.gui;
-
-import javax.swing.JFrame;
-import javax.swing.UIManager;
-import javax.swing.UIManager.LookAndFeelInfo;
-
-@SuppressWarnings("serial")
-public class MainFrame extends JFrame {
-
-	private RobotPanel robot;
-
-	/**
-	 * Create the frame.
-	 */
-	public MainFrame() {
-//	    try {
-//	        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-//	    } catch (Exception e) {
-//	        e.printStackTrace();
-//	    }
-		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1400, 1000);
-		setLocationRelativeTo(null);
-		robot = new RobotPanel();
-		robot.setAlignmentX(200);
-		add(robot);
-//		add(new RobotPanel());
-//		add(new RobotPanel());
-
-
-	}
-
-}
->>>>>>> 9d27cad64a3d481f8744330f844b8c012bfd57f5
