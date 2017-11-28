a32bf0286b8f5e3045b1322371f6532226b8eb72
ramachandra <ajay@ajayinkingston.com>
2017-11-27 20:27:41 -0500

    added an insane amount of stuff

--- src/ca/lakeeffectrobotics/scoutingreader/gui/DataSelection.java
+++ src/ca/lakeeffectrobotics/scoutingreader/gui/DataSelection.java
@@ -1,19 +1,20 @@
 package ca.lakeeffectrobotics.scoutingreader.gui;
 
 import java.awt.BorderLayout;
-import java.awt.Checkbox;
 import java.awt.Component;
 import java.awt.Font;
-import java.awt.List;
+import java.awt.TextField;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
-import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
@@ -22,7 +23,6 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
-import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
@@ -30,25 +30,37 @@ import ca.lakeeffectrobotics.scoutingreader.Main;
 import ca.lakeeffectrobotics.scoutingreader.Match;
 import ca.lakeeffectrobotics.scoutingreader.Robot;
 
-public class DataSelection extends JFrame implements ItemListener, MouseListener{
+public class DataSelection extends JFrame implements ItemListener, MouseListener, KeyListener{
 	
-	JPanel contentPane;
+	/**Main JPanel*/
+	JPanel contentPanel;
 	
-	int selectedRobot = 0;
-	JComboBox<Integer> robotSelector;
+	int selectedRobot = 1075;
 	
 	static JFrame frame;
 	
+	/**JPanel with match selection checkboxes*/
 	JPanel matchPanel;
+	/**JPanel with data selection checkboxes*/
 	JPanel dataPanel;
+	/**JPanel with robot selection checkboxes*/
+	JPanel robotNumberPanel;
+	
 	
 	JCheckBox allMatches;
+	/**Check box to select all data*/
 	JCheckBox allData;
-	JButton clearDataButton;
+	
+	TextField robotSearch;
+	
+	/** Match view type selector */
+	JComboBox<Integer> matchViewTypeSelector;
 	
 	Main main;
+	
 	ArrayList<Integer> selectedMatches = new ArrayList<>();
 	ArrayList<String> selectedLabels = new ArrayList<>();
+	ArrayList<String> selectedRobots = new ArrayList<>();
 	
 	//the checkboxes sorted into seperate lists
 	ArrayList<JCheckBox> matchBoxes = new ArrayList<>();
@@ -68,44 +80,14 @@ public class DataSelection extends JFrame implements ItemListener, MouseListener
 	        e.printStackTrace();
 	    }
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setLayout(new BorderLayout(0, 0));
-		contentPane.setBorder(null);
-		setContentPane(contentPane);
+		setSize(1000, 500);
+		setTitle("Data Selection");
 		
-		
-		JPanel bottomPanel = new JPanel();
-		bottomPanel.setBorder(null);
-		contentPane.add(bottomPanel, BorderLayout.SOUTH);
-		
-
-		bottomPanel.setLayout(new BorderLayout(0,0));
-		
-		allMatches = new JCheckBox("All Matches");
-		bottomPanel.add(allMatches, BorderLayout.WEST);
-		allMatches.addItemListener(this);
-		
-		allData = new JCheckBox("All Data");
-		bottomPanel.add(allData, BorderLayout.EAST);
-		allData.addItemListener(this);
-		
-		
-		clearDataButton = new JButton("Clear Data");
-		bottomPanel.add(clearDataButton, BorderLayout.CENTER);
-
-		
-		JSplitPane matchPane = new JSplitPane();
-		matchPane.setBorder(null);
-		matchPane.setDividerSize(0);
-		matchPane.setResizeWeight(0.1);
-		contentPane.add(matchPane, BorderLayout.NORTH);
-		
-		JLabel lblNewLabel = new JLabel("Robot:");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		matchPane.setLeftComponent(lblNewLabel);
-		
-		robotSelector = new JComboBox<Integer>();
+		//create the content JPanel
+		contentPanel = new JPanel();
+		contentPanel.setLayout(new BorderLayout(0, 0));
+		contentPanel.setBorder(null);
+		setContentPane(contentPanel);
 		
 		Integer[] robots = new Integer[main.robots.size()];
 		
@@ -113,67 +95,128 @@ public class DataSelection extends JFrame implements ItemListener, MouseListener
 			robots[i] = main.robots.get(i).number;
 		}
 		Arrays.sort(robots);
-		
-		robotSelector.setModel(new DefaultComboBoxModel<Integer>(robots));
-		robotSelector.setToolTipText("");
-		matchPane.setRightComponent(robotSelector);
 
+		/** Main split pane */
+		JSplitPane splitPane = new JSplitPane();
+		splitPane.setResizeWeight(0.5);
+		splitPane.setBorder(null);
+		splitPane.setDividerSize(0);
+	
 		
-		JSplitPane buttonPane = new JSplitPane();
-		buttonPane.setResizeWeight(0.5);
-		buttonPane.setBorder(null);
-		buttonPane.setDividerSize(0);
-		contentPane.add(buttonPane, BorderLayout.CENTER);
+		//add the split pane to the window 
+		contentPanel.add(splitPane, BorderLayout.CENTER);
+		
+		JLabel title = new JLabel();
+		title.setFont(new Font("Tahoma", Font.BOLD, 23));
+		title.setText("Data Selection");
+		title.setHorizontalAlignment(JLabel.CENTER);
+		contentPanel.add(title, BorderLayout.NORTH);
+		
+		/** split pane containing robots and matches */
+		JSplitPane robotSplitPane = new JSplitPane();
+		robotSplitPane.setResizeWeight(0.5);
+		robotSplitPane.setBorder(null);
+		robotSplitPane.setDividerSize(0);
+		splitPane.add(robotSplitPane, JSplitPane.LEFT);
+		
+		/** splitpane containg match numbers and spinner */
+		JSplitPane matchPane = new JSplitPane();
+		matchPane.setResizeWeight(0.95);
+		matchPane.setBorder(null);
+		matchPane.setDividerSize(0);
+		matchPane.setOrientation(JSplitPane.VERTICAL_SPLIT);
+		robotSplitPane.add(matchPane, JSplitPane.RIGHT);
+		
+		/** splitpane containg data and checkboxes all data and all robots */
+		JSplitPane dataPane = new JSplitPane();
+		dataPane.setResizeWeight(0.95);
+		dataPane.setBorder(null);
+		dataPane.setDividerSize(0);
+		dataPane.setOrientation(JSplitPane.VERTICAL_SPLIT);
+		splitPane.add(dataPane, JSplitPane.RIGHT);
+		
+		/** splitpane containg robot numbers and search box */
+		JSplitPane robotNumberPane = new JSplitPane();
+		robotNumberPane.setResizeWeight(0.95);
+		robotNumberPane.setBorder(null);
+		robotNumberPane.setDividerSize(0);
+		robotNumberPane.setOrientation(JSplitPane.VERTICAL_SPLIT);
+		robotSplitPane.add(robotNumberPane, JSplitPane.LEFT);
+		
 		
 		JScrollPane leftPane = new JScrollPane();
+		robotNumberPane.add(leftPane, JSplitPane.TOP);
 		leftPane.getVerticalScrollBar().setUnitIncrement(20);
-		buttonPane.setLeftComponent(leftPane);
 		
-		matchPanel = new JPanel();
-		leftPane.setViewportView(matchPanel);
-		matchPanel.setLayout(new BoxLayout(matchPanel, BoxLayout.Y_AXIS));
-		
-		JLabel lblMatch = new JLabel("Match");
-		lblMatch.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblMatch.setAlignmentX(Component.CENTER_ALIGNMENT);
-		lblMatch.setHorizontalAlignment(SwingConstants.CENTER);
-		matchPanel.add(lblMatch);
-		
+		JScrollPane centerPane = new JScrollPane();
+		matchPane.add(centerPane, JSplitPane.TOP);
+		centerPane.getVerticalScrollBar().setUnitIncrement(20);
 		
 		JScrollPane rightPane = new JScrollPane();
+		dataPane.add(rightPane, JSplitPane.TOP);
 		rightPane.getVerticalScrollBar().setUnitIncrement(20);
-		buttonPane.setRightComponent(rightPane);
-				dataPanel = new JPanel();
+		
+		robotNumberPanel = new JPanel();
+		leftPane.setViewportView(robotNumberPanel);
+		robotNumberPanel.setLayout(new BoxLayout(robotNumberPanel, BoxLayout.Y_AXIS));
+		
+		
+		robotSearch = new TextField();
+		robotSearch.addKeyListener(this);
+		robotNumberPane.add(robotSearch, JSplitPane.BOTTOM);
+		
+		matchPanel = new JPanel();
+		centerPane.setViewportView(matchPanel);
+		matchPanel.setLayout(new BoxLayout(matchPanel, BoxLayout.Y_AXIS));
+		
+		JButton clearRobotNumberButton = new JButton("Clear Robot Numbers");
+		matchPane.setRightComponent(clearRobotNumberButton);
+		
+		// Add all matches and all robots checkboxes
+		JSplitPane allSelectionPane = new JSplitPane();
+		allSelectionPane.setResizeWeight(0.5);
+		allSelectionPane.setBorder(null);
+		allSelectionPane.setDividerSize(0);
+		dataPane.add(allSelectionPane, JSplitPane.BOTTOM);
+		
+		allData = new JCheckBox();
+		allData.setText("All data");
+		allData.setHorizontalAlignment(JCheckBox.CENTER);
+		allData.addItemListener(this);
+		allSelectionPane.add(allData, JSplitPane.RIGHT);
+		
+		allMatches = new JCheckBox();
+		allMatches.setSelected(true);
+		allMatches.setText("All matches");
+		allMatches.setHorizontalAlignment(JCheckBox.CENTER);
+		allMatches.addItemListener(this);
+		allSelectionPane.add(allMatches, JSplitPane.LEFT);
+		
+		dataPanel = new JPanel();
 		rightPane.setViewportView(dataPanel);
 		dataPanel.setLayout(new BoxLayout(dataPanel, BoxLayout.Y_AXIS));
 		
-		JLabel lblData = new JLabel("Data");
-		lblData.setHorizontalAlignment(SwingConstants.CENTER);
-		lblData.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblData.setAlignmentX(0.5f);
-		dataPanel.add(lblData);
+
 		
-		robotSelector.addItemListener(this);
-		clearDataButton.addMouseListener(this);
-		
+		resetRobotBoxes();
 		setVisible(true);
 		setOptions();
 	}
 
 	@Override
 	public void itemStateChanged(ItemEvent itemEvent) {
-		if(itemEvent.getSource() == robotSelector){
-			loadingNewRobot = true;
-			
-			//Old robot that was previously selected
-			if(itemEvent.getStateChange() == ItemEvent.DESELECTED) return;
-			//The same robot
-			if((Integer) robotSelector.getSelectedItem() == selectedRobot) return;
-			//Setup checkboxes
-			getSelectedData();
-			setOptions();
-			
-			loadingNewRobot = false;
+		if(itemEvent.getSource() == null){
+//			loadingNewRobot = true;
+//			
+//			//Old robot that was previously selected
+//			if(itemEvent.getStateChange() == ItemEvent.DESELECTED) return;
+//			//The same robot
+//			if((Integer) robotSelector.getSelectedItem() == selectedRobot) return;
+//			//Setup checkboxes
+//			getSelectedData();
+//			setOptions();
+//			
+//			loadingNewRobot = false;
 		} else if(itemEvent.getSource() == allMatches){
 			if(itemEvent.getStateChange() == ItemEvent.DESELECTED){
 				for(Component c : matchPanel.getComponents()){
@@ -225,10 +268,9 @@ public class DataSelection extends JFrame implements ItemListener, MouseListener
 					allData.setSelected(false);
 					allData.addItemListener(this);
 				}
-				
 			}
 			
-			getSelectedData();
+			getSelectedData(( (JCheckBox) itemEvent.getSource()).getText() );
 		}
 	}
 	
@@ -237,8 +279,9 @@ public class DataSelection extends JFrame implements ItemListener, MouseListener
 	 */
 	
 	void setOptions(){
-		selectedRobot = (Integer) robotSelector.getSelectedItem();
+//		selectedRobot = (Integer) robotSelector.getSelectedItem();
 		
+		//TODO: Replace selected robot with newer system
 		System.out.println(selectedRobot);
 		
 		Robot robot = main.getRobotByNumber(selectedRobot);
@@ -250,12 +293,25 @@ public class DataSelection extends JFrame implements ItemListener, MouseListener
 		dataPanel.removeAll();
 		
 		//Create match checkboxes
-		for(Match m : robot.matches){
-			System.out.println(m.number);
-			JCheckBox checkBox = new JCheckBox("Match: " + m.number);
-			matchPanel.add(checkBox);	
-			checkBox.addItemListener(this);
-			matchBoxes.add(checkBox);
+		matchPanel.removeAll();
+		for(String robotNum: selectedRobots){
+			
+			robot = main.getRobotByNumber(Integer.parseInt(robotNum.replace("Robot: ", "")));
+			JLabel robotLabel = new JLabel();
+			robotLabel.setHorizontalTextPosition(JLabel.LEFT);
+			robotLabel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 0));
+			robotLabel.setText(robotNum);
+			matchPanel.add(robotLabel);
+			
+			for(Match m : robot.matches){
+				System.out.println(m.number);
+				JCheckBox checkBox = new JCheckBox("Match: " + m.number);
+				if(allMatches.isSelected()) checkBox.setSelected(true);
+				checkBox.setBorder(BorderFactory.createEmptyBorder(0, 30, 10, 0));
+				matchPanel.add(checkBox);	
+				checkBox.addItemListener(this);
+				matchBoxes.add(checkBox);
+			}
 		}
 		
 		//Create data checkboxes
@@ -293,7 +349,7 @@ public class DataSelection extends JFrame implements ItemListener, MouseListener
 		SwingUtilities.updateComponentTreeUI(this);
 	}
 	
-	void getSelectedData(){
+	void getSelectedData(String checkBoxLabel){
 		selectedMatches = new ArrayList<Integer>();
 		
 		for(Component c : matchPanel.getComponents()){
@@ -320,67 +376,128 @@ public class DataSelection extends JFrame implements ItemListener, MouseListener
 			}
 		}
 		
+		selectedRobots = new ArrayList<String>();
+		
+		for(Component c : robotNumberPanel.getComponents()){
+			if(!(c instanceof JCheckBox)) continue;
+
+			JCheckBox box = ((JCheckBox) c);
+			
+			if(box.isSelected()){
+				System.out.println(box.getText());
+				selectedRobots.add(box.getText());
+			}
+		}
+		
 		//Display data
 		Robot r = main.getRobotByNumber(selectedRobot);
 		for(RobotPanel robotPanel: main.frame.robotPanels){
 			robotPanel.loadData(r, selectedMatches, selectedLabels);
 
 		}
+		
+		if(checkBoxLabel.contains("Robot:")) setOptions(); //it must be a change of robots
 	}
 	
-	public void clearBoxes(){
-		for(Component c : matchPanel.getComponents()){
-			if(!(c instanceof JCheckBox)) continue;
-
-			JCheckBox box = ((JCheckBox) c);
-			
-			box.setSelected(false);
-		}
-		for(Component c : dataPanel.getComponents()){
-			if(!(c instanceof JCheckBox)) continue;
-
-			JCheckBox box = ((JCheckBox) c);
-			
-			box.setSelected(false);
-		}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		String text = robotSearch.getText();
 		
+		if(e.getSource() == robotSearch && !text.equals("")){ 
+			
+			ArrayList<Integer> containedRobotNumbers = new ArrayList<>();
+			
+			for(Robot robot: main.robots){
+				if((robot.number + "").startsWith(text)){
+					containedRobotNumbers.add(robot.number);
+				}
+
+			}
+			
+			resetRobotBoxes();
+			
+			ArrayList<Component> componentsToRemove = new ArrayList<>();
+			
+			for(Component c : robotNumberPanel.getComponents()){
+				if(!(c instanceof JCheckBox)) continue;
+
+				JCheckBox box = ((JCheckBox) c);
+				
+				if(!containedRobotNumbers.contains(Integer.parseInt(box.getText().replace("Robot: ", "")))){
+					componentsToRemove.add(box);
+				}
+			}
+			
+			for(Component component: componentsToRemove){
+				robotNumberPanel.remove(component);
+			}
+			
+			SwingUtilities.updateComponentTreeUI(this);
+			
+		}else if(e.getSource() == robotSearch && text.equals("")){
+			resetRobotBoxes();
+		}
 	}
 
+	
+	private void resetRobotBoxes(){
+		robotNumberPanel.removeAll();
+		
+		Integer[] robots = new Integer[main.robots.size()];
+		
+		for(int i=0;i<robots.length;i++){
+			robots[i] = main.robots.get(i).number;
+		}
+		
+		Arrays.sort(robots);
+		
+		for(int robot: robots){
+			JCheckBox checkBox = new JCheckBox("Robot: " + robot);
+			
+			robotNumberPanel.add(checkBox);	
+			checkBox.addItemListener(this);
+		}
+		
+		SwingUtilities.updateComponentTreeUI(this);
+	}
+	
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		if(e.getComponent() == clearDataButton){
-			System.out.println("Clear Data");
-			clearBoxes();
-		}
+		
 	}
 	
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
 		
 	}
 
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
 		
 	}
 
 
 	@Override
 	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
 		
 	}
 
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
 		
 	}
-
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+	}
 
 }
